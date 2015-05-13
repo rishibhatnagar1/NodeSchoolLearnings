9aa409c16f7735b7ab9d0843ad24f742b6000ae5
rishibhatnagar1 <rishibhatnagar1@gmail.com>
2015-05-13 23:53:20 +0530

    till tell template

---
+++ Lynda_NodeEssentials/gettingstartedExpress/helloExp/app.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app = express()
+
+app.set('view engine','ejs');// setups up variables for express
+/* if I wish to use some other directory instead of view, this is how it is done 
+app.set('view',__dirname+ '/');
+
+*/ 
+app.get('/',function(req,res){
+//res.send("<h1>Hello</h1> Express <h2>Bitch!</h2>");
+    res.render('default',{
+        title:'Home',
+        }); // This is used for rendering a template etc.
+})
+/* these are all routes, the above one is the root route, everything else has a different route, which can be used for various purposes. */
+app.get('/me',function(req,res){
+res.send("@rishigb");
+})
+
+app.get('/who/:name',function(req,res){
+var name = req.params.name;
+res.send("@" +name + "May or may not exist. I am just saying bro.");
+})
+
+app.get('*',function(req,res){
+res.send("NOT FOUND BRO");
+})
+
+var server = app.listen(3000,function(){
+console.log('listening on port 3000 ');
+})
